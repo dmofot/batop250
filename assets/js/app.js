af08bb265faa073318162a9609d904279471a965
DT <dmofot@gmail.com>
2014-07-09 17:03:47 -0400

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,339 @@
+var map, beerSearch = [];
+
+$(document).ready(function() {
+  getViewport();
+});
+
+function getViewport() {
+  if (sidebar.isVisible()) {
+    map.setActiveArea({
+      position: "absolute",
+      top: "0px",
+      left: $(".leaflet-sidebar").css("width"),
+      right: "0px",
+      height: $("#map").css("height")
+    });
+  } else {
+    map.setActiveArea({
+      position: "absolute",
+      top: "0px",
+      left: "0px",
+      right: "0px",
+      height: $("#map").css("height")
+    });
+  }
+  if (document.body.clientWidth <= 767) {
+    $(".leaflet-sidebar .close").css("top", "8px");
+  } else {
+    $(".leaflet-sidebar .close").css("top", "15px");
+  }
+}
+
+function sidebarClick(id) {
+  /* If sidebar takes up entire screen, hide it and go to the map */
+  if (document.body.clientWidth <= 767) {
+    sidebar.hide();
+    getViewport();
+  }
+  map.addLayer(beerLayer);
+  var layer = markerClusters.getLayer(id);
+  markerClusters.zoomToShowLayer(layer, function() {
+    map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 18);
+    layer.fire("click");
+  });
+}
+
+/* Basemap Layers */
+var mapboxTiles = L.tileLayer("https://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png", {
+  attribution: 'Tiles courtesy of <a href="http://www.mapbox.com/" target="_blank">Mapbox</a> '
+});
+
+/* Overlay Layers */
+var highlight = L.geoJson(null);
+
+/* Single marker cluster layer to hold all clusters */
+var markerClusters = new L.MarkerClusterGroup({
+  spiderfyOnMaxZoom: true,
+  showCoverageOnHover: false,
+  zoomToBoundsOnClick: true,
+  disableClusteringAtZoom: 16
+});
+
+/* Empty layer placeholder to add to layer control for listening when to add/remove theaters to markerClusters layer */
+var beerLayer = L.geoJson(null);
+var beers = L.geoJson(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: "assets/img/beer.png",
+        iconSize: [24, 28],
+        iconAnchor: [12, 28],
+        popupAnchor: [0, -25]
+      }),
+      title: feature.properties.name,
+      riseOnHover: true
+    });
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.name + "</td></tr>" + "<tr><th>Rank</th><td>" + feature.properties.rank + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.address + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.homepage + "' target='_blank'>" + feature.properties.homepage + "</a></td></tr>" + "<table>";
+      layer.on({
+        click: function (e) {
+          $("#feature-title").html(feature.properties.name);
+          $("#feature-info").html(content);
+          $("#featureModal").modal("show");
+          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+            stroke: false,
+            fillColor: "#00FFFF",
+            fillOpacity: 0.7,
+            radius: 10
+          }));
+        }
+      });
+      $("#beer-table tbody").append('<tr style="cursor: pointer;" onclick="sidebarClick('+L.stamp(layer)+'); return false;"><td class="beer-name">'+layer.feature.properties.name+'<i class="fa fa-chevron-right pull-right"></td></tr>');
+      beerSearch.push({
+        name: layer.feature.properties.name,
+        address: layer.feature.properties.address,
+        source: "Beers",
+        id: L.stamp(layer),
+        lat: layer.feature.geometry.coordinates[1],
+        lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+  }
+});
+$.getJSON("data/beers.geojson", function (data) {
+  beers.addData(data);
+  map.addLayer(beerLayer);
+});
+
+map = L.map("map", {
+  zoom: 10,
+  center: [42.48814, -71.25861],
+  layers: [mapboxTiles, beers, markerClusters, highlight],
+  zoomControl: false,
+  attributionControl: false
+});
+
+/* Layer control listeners that allow for a single markerClusters layer */
+map.on("overlayadd", function(e) {
+  if (e.layer === beerLayer) {
+    markerClusters.addLayer(beers);
+  }
+});
+
+map.on("overlayremove", function(e) {
+  if (e.layer === beerLayer) {
+    markerClusters.removeLayer(beers);
+  }
+});
+
+/* Clear feature highlight when featureModal is closed */
+$("#featureModal").on("hide.bs.modal", function (e) {
+  highlight.clearLayers();
+});
+
+/* Attribution control */
+function updateAttribution(e) {
+  $.each(map._layers, function(index, layer) {
+    if (layer.getAttribution) {
+      $("#attribution").html((layer.getAttribution()));
+    }
+  });
+}
+map.on("layeradd", updateAttribution);
+map.on("layerremove", updateAttribution);
+
+var attributionControl = L.control({
+  position: "bottomright"
+});
+attributionControl.onAdd = function (map) {
+  var div = L.DomUtil.create("div", "leaflet-control-attribution");
+  div.innerHTML = "Modified by <a href='http://davidtodd.info'>davidtodd.info</a> | <a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
+  return div;
+};
+map.addControl(attributionControl);
+
+var zoomControl = L.control.zoom({
+  position: "bottomright"
+}).addTo(map);
+
+/* GPS enabled geolocation control set to follow the user's location */
+var locateControl = L.control.locate({
+  position: "bottomright",
+  drawCircle: true,
+  follow: true,
+  setView: true,
+  keepCurrentZoomLevel: true,
+  markerStyle: {
+    weight: 1,
+    opacity: 0.8,
+    fillOpacity: 0.8
+  },
+  circleStyle: {
+    weight: 1,
+    clickable: false
+  },
+  icon: "icon-direction",
+  metric: false,
+  strings: {
+    title: "My location",
+    popup: "You are within {distance} {unit} from this point",
+    outsideMapBoundsMsg: "You seem located outside the boundaries of the map"
+  },
+  locateOptions: {
+    maxZoom: 17,
+    watch: true,
+    enableHighAccuracy: true,
+    maximumAge: 10000,
+    timeout: 10000
+  }
+}).addTo(map);
+
+var sidebar = L.control.sidebar("sidebar", {
+  closeButton: true,
+  position: "left"
+}).on("shown", function () {
+  getViewport();
+}).on("hidden", function () {
+  getViewport();
+}).addTo(map);
+
+/* Larger screens get expanded layer control and visible sidebar */
+if (document.body.clientWidth <= 767) {
+  var isCollapsed = true;
+} else {
+  var isCollapsed = false;
+  sidebar.show();
+}
+
+var baseLayers = {
+  "Street Map": mapboxTiles
+};
+
+var groupedOverlays = {
+  "Points of Interest": {
+    "<img src='assets/img/beer.png' width='24' height='28'>&nbsp;Beers": beerLayer
+  }
+};
+
+var layerControl = L.control.groupedLayers(baseLayers, groupedOverlays, {
+  collapsed: isCollapsed
+}).addTo(map);
+
+/* Highlight search box text on click */
+$("#searchbox").click(function () {
+  $(this).select();
+});
+
+/* Typeahead search functionality */
+$(document).one("ajaxStop", function () {
+  /* Fit map to brews bounds */
+  map.fitBounds(beers.getBounds());
+  $("#loading").hide();
+
+  var beersBH = new Bloodhound({
+    name: "Beers",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: beerSearch,
+    limit: 10
+  });
+  var beerList = new List("beers", {valueNames: ["beer-name", "beer-address"]}).sort("beer-name", {order:"asc"});
+
+  var geonamesBH = new Bloodhound({
+    name: "GeoNames",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: "http://api.geonames.org/searchJSON?username=dmofot&featureClass=P&maxRows=5&countryCode=US&name_startsWith=%QUERY",
+      filter: function (data) {
+        return $.map(data.geonames, function (result) {
+          return {
+            name: result.name + ", " + result.adminCode1,
+            lat: result.lat,
+            lng: result.lng,
+            source: "GeoNames"
+          };
+        });
+      },
+      ajax: {
+        beforeSend: function (jqXhr, settings) {
+          settings.url += "&east=" + map.getBounds().getEast() + "&west=" + map.getBounds().getWest() + "&north=" + map.getBounds().getNorth() + "&south=" + map.getBounds().getSouth();
+          $("#searchicon").removeClass("fa-search").addClass("fa-refresh fa-spin");
+        },
+        complete: function (jqXHR, status) {
+          $('#searchicon').removeClass("fa-refresh fa-spin").addClass("fa-search");
+        }
+      }
+    },
+    limit: 10
+  });
+  beersBH.initialize();
+  geonamesBH.initialize();
+
+  /* instantiate the typeahead UI */
+  $("#searchbox").typeahead({
+    minLength: 3,
+    highlight: true,
+    hint: false
+  }, {
+    name: "Beers",
+    displayKey: "name",
+    source: brewsBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'><img src='assets/img/beer.png' width='24' height='28'>&nbsp;Beers</h4>",
+      suggestion: Handlebars.compile(["{{name}}<br>&nbsp;<small>{{address}}</small>"].join(""))
+    }
+  }, {
+    name: "GeoNames",
+    displayKey: "name",
+    source: geonamesBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'><img src='assets/img/globe.png' width='25' height='25'>&nbsp;GeoNames</h4>"
+    }
+  }).on("typeahead:selected", function (obj, datum) {
+    if (datum.source === "Beers") {
+      if (!map.hasLayer(beerLayer)) {
+        map.addLayer(beerLayer);
+      }
+      map.setView([datum.lat, datum.lng], 17);
+      if (map._layers[datum.id]) {
+        map._layers[datum.id].fire("click");
+      }
+    }
+    if (datum.source === "GeoNames") {
+      map.setView([datum.lat, datum.lng], 14);
+    }
+    if ($(".navbar-collapse").height() > 50) {
+      $(".navbar-collapse").collapse("hide");
+    }
+  }).on("typeahead:opened", function () {
+    $(".navbar-collapse.in").css("max-height", $(document).height() - $(".navbar-header").height());
+    $(".navbar-collapse.in").css("height", $(document).height() - $(".navbar-header").height());
+  }).on("typeahead:closed", function () {
+    $(".navbar-collapse.in").css("max-height", "");
+    $(".navbar-collapse.in").css("height", "");
+  });
+  $(".twitter-typeahead").css("position", "static");
+  $(".twitter-typeahead").css("display", "block");
+});
+
+/* Placeholder hack for IE */
+if (navigator.appName == "Microsoft Internet Explorer") {
+  $("input").each(function () {
+    if ($(this).val() === "" && $(this).attr("placeholder") !== "") {
+      $(this).val($(this).attr("placeholder"));
+      $(this).focus(function () {
+        if ($(this).val() === $(this).attr("placeholder")) $(this).val("");
+      });
+      $(this).blur(function () {
+        if ($(this).val() === "") $(this).val($(this).attr("placeholder"));
+      });
+    }
+  });
+}
